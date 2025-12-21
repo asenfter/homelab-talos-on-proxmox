9fb3f11ee50f84d7f6e2221dd63ae93605bf2d2d
Andreas Senfter <andreas@senfter.net>
2025-12-21 15:37:22 +0000

    add kubeseal

--- .devcontainer/on_create.sh
+++ .devcontainer/on_create.sh
@@ -24,6 +24,19 @@ rm -f kubectl kubectl.sha256
 curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 helm version
 # ----------------------------------------------------------------
+# Install kubeseal
+# Fetch the latest sealed-secrets version using GitHub API
+KUBESEAL_VERSION=$(curl -s https://api.github.com/repos/bitnami-labs/sealed-secrets/tags | jq -r '.[0].name' | cut -c 2-)
+# Check if the version was fetched successfully
+if [ -z "$KUBESEAL_VERSION" ]; then
+    echo "Failed to fetch the latest KUBESEAL_VERSION"
+    exit 1
+fi
+curl -OL "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${KUBESEAL_VERSION}/kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz"
+tar -xvzf kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz kubeseal
+sudo install -m 755 kubeseal /usr/local/bin/kubeseal
+rm -f kubeseal-${KUBESEAL_VERSION}-linux-amd64.tar.gz kubeseal
+# ----------------------------------------------------------------
 # Install OpenTofu CLI
 
 # Download the installer script:
